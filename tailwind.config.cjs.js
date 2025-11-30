a0b39052632d0ec8bd30fcd3da861b0adfd407c4
ljjliujunjie <ljjliujunjie@gmail.com>
2025-11-30 23:23:39 +0800

    Initial code upload for Appflow

---
+++ tailwind.config.cjs.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 确保包含所有 JSX/JS 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
